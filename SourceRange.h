d369fc438c7c64c64d9bf6e0273092237b60aafb
Igor Pyanzin <igor.pyanzin@gmail.com>
2017-08-10 18:11:41 +0300

    SourceRange wip

---
+++ SourceRange.h
@@ -0,0 +1,40 @@
+﻿#include <string>
+#include <tuple>
+
+class SourceRange {
+public:
+	SourceRange(std::string* src, int start = -1, int end = -1) {
+		_start = start;
+		_end = end;
+		_src = src;
+	}
+
+	int getStart() {
+		return _start;
+	}
+
+	int getEnd() {
+		return _end;
+	}
+
+	std::tuple<int, int> getRowCol(int position) {
+		int row = 1;
+		int col = 1;
+
+		int i = 0;
+		while (i++ != position)
+			if ((*_src)[i] == '\n') {
+				++row;
+				col = 1;
+			} else {
+				++col;
+			}		
+
+		return std::make_tuple(row, col);
+	}
+
+private:
+	int _start;
+	int _end;
+	std::string* _src;
+};
