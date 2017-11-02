b79aca28f9375c5e673e032fb920c7c6209f2830
Igor Pyanzin <igor.pyanzin@gmail.com>
2017-11-02 18:22:40 +0300

    for statement, wip

---
+++ ZFor.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "ZAst.h"
+#include "ZExpr.h"
+
+class ZFor : public ZAst {
+public:
+
+	ZFor(ZAst* body, ZAst* pre = nullptr, ZExpr* cond = nullptr, ZExpr* post = nullptr) {
+		_body = body;
+		_pre = pre;
+		_cond = cond;
+		_post = post;
+	}
+
+	ZAst* getBody() {
+		return _body;
+	}
+
+	ZAst* getPre() {
+		return _pre;
+	}
+
+	ZExpr* getCond() {
+		return _cond;
+	}
+
+	ZExpr* getPost() {
+		return _post;
+	}
+
+private:
+	ZAst* _body;
+	ZAst* _pre;
+	ZExpr* _cond;
+	ZExpr* _post;
+};
