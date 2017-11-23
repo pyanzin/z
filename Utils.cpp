8a2f78f775bc2c63fcffe76332ad1fb1f966bc0a
Igor Pyanzin <igor.pyanzin@gmail.com>
2017-11-23 16:56:06 +0300

    toString for new lexemes

--- Utils.cpp
+++ Utils.cpp
@@ -1,43 +1,50 @@
-﻿#include "ZBasicType.h"
-#include <llvm/IR/LLVMContext.h>
-#include <llvm/IR/DerivedTypes.h>
-#include "ZLexeme.h"
+﻿#include "ZLexeme.h"
 #include "ZBinOp.h"
 
 std::string toString(ZLexeme lexeme) {
 	switch (lexeme) {
-	case IDENT:         return "IDENT";
-	case DEF:           return "DEF";
-	case EXTERN:        return "EXTERN";
-	case VAR:           return "VAR";
-	case RETURN:        return "RETURN";
-	case IF:            return "IF";
-	case ELSE:          return "ELSE";
-	case WHILE:         return "WHILE";
-	case STRING_LIT:    return "STRING_LIT";
-	case INT_LIT:       return "INT_LIT";
-	case CHAR_LIT:      return "CHAR_LIT";
-	case OPEN_PAREN:    return "OPEN_PAREN";
-	case CLOSE_PAREN:   return "CLOSE_PAREN";
-	case COLON:         return "COLON";
-	case SEMICOLON:     return "SEMICOLON";
-	case EQUAL:         return "EQUAL";
-	case DOUBLE_EQUAL:  return "DOUBLE_EQUAL";
-	case NOT_EQUAL:     return "NOT_EQUAL";
-	case LESS:          return "LESS";
-	case LESS_OR_EQUAL: return "LESS_OR_EQUAL";
-	case MORE:          return "MORE";
-	case MORE_OR_EQUAL: return "MORE_OR_EQUAL";
-	case OPEN_BRACE:    return "OPEN_BRACE";
-	case CLOSE_BRACE:   return "CLOSE_BRACE";
-	case FAT_ARROW:     return "FAT_ARROW";
-	case COMMA:         return "COMMA";
-	case PLUS:          return "PLUS";
-	case MINUS:         return "MINUS";
-	case ASTERISK:      return "ASTERISK";
-	case EXCLAM:        return "EXCLAM";
-	case SLASH:         return "SLASH";
-	case INPUT_END:     return "INPUT_END";
+	case IDENT:          return "IDENT";
+	case DEF:            return "DEF";
+	case STRUCT:         return "STRUCT";
+	case EXTERN:         return "EXTERN";
+	case VAR:            return "VAR";
+	case RETURN:         return "RETURN";
+	case IF:			 return "IF";
+	case ELSE:           return "ELSE";
+	case WHILE:          return "WHILE";
+	case FOR:            return "FOR";
+	case STRING_LIT:     return "STRING_LIT";
+	case INT_LIT:        return "INT_LIT";
+	case CHAR_LIT:       return "CHAR_LIT";
+	case BOOL_TRUE_LIT:  return "BOOL_TRUE_LIT";
+	case BOOL_FALSE_LIT: return "BOOL_FALSE_LIT";
+	case OPEN_PAREN:     return "OPEN_PAREN";
+	case CLOSE_PAREN:    return "CLOSE_PAREN";
+	case COLON:          return "COLON";
+	case SEMICOLON:      return "SEMICOLON";
+	case EQUAL:          return "EQUAL";
+	case DOUBLE_EQUAL:   return "DOUBLE_EQUAL";
+	case NOT_EQUAL:      return "NOT_EQUAL";
+	case LESS:           return "LESS";
+	case LESS_OR_EQUAL:  return "LESS_OR_EQUAL";
+	case MORE:           return "MORE";
+	case MORE_OR_EQUAL:  return "MORE_OR_EQUAL";
+	case OPEN_BRACE:     return "OPEN_BRACE";
+	case CLOSE_BRACE:    return "CLOSE_BRACE";
+	case OPEN_BRACKET:   return "OPEN_BRACKET";
+	case CLOSE_BRACKET:  return "CLOSE_BRACKET";
+	case FAT_ARROW:      return "FAT_ARROW";
+	case COMMA:          return "COMMA";
+	case DOT:            return "DOT";
+	case PLUS:           return "PLUS";
+	case MINUS:          return "MINUS";
+	case ASTERISK:       return "ASTERISK";
+	case EXCLAM:         return "EXCLAM";
+	case SLASH:          return "SLASH";
+	case SIZEOF:         return "SIZEOF";
+	case AS:             return "AS";
+	case INPUT_END:      return "INPUT_END";
+	default:			 return "UNKNOWN_LEXEME";
 
 	}
 
