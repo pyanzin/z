6159864eef4f500ba3bdebeb00ae2c2278801eb4
Igor Pyanzin <igor.pyanzin@gmail.com>
2017-11-28 22:43:04 +0300

    parser errors collecting

---
+++ JuxtaposeResult.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "ZType.h"
+
+struct JuxtaposeResult {
+    JuxtaposeResult(ZType* type = nullptr, bool advances = false, bool completed = false) {
+        this->type = type;
+        this->advances = advances;
+        this->completed = completed;
+    }
+
+    ZType* type;
+    bool advances;
+    bool completed;
+};
