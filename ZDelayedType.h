4a727cf7bbd6c2eb7f8b4e999f9b9bb33aae706e
Igor Pyanzin <igor.pyanzin@gmail.com>
2017-11-28 18:56:55 +0300

    delayed type surrogate

---
+++ ZDelayedType.h
@@ -0,0 +1,26 @@
+﻿#include "ZType.h"
+
+class ZDelayedType : public ZType {
+public:
+	ZDelayedType(std::string& delayedName) : _delayedName(delayedName) {  }
+	llvm::Type* toLlvmType() override {
+		throw "Error: ZDelayedType is supposed to be resolved before LLVM pass";
+	}
+
+	std::string& getName() override {
+		return *new std::string("DelayedType");
+	}
+	std::string& toString() override {
+		return *new std::string("DelayedType");
+	}
+
+	bool isEqual(ZType& other) override {
+		return false;
+	}
+
+	std::string& getDelayedName() {
+		return _delayedName;
+	}
+private:
+	std::string _delayedName;
+};
