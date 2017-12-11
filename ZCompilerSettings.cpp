81e97809aa43f33f19ac395f22a844ca986ca069
Igor Pyanzin <igor.pyanzin@gmail.com>
2017-12-11 15:48:32 +0300

    parsing of CL params

---
+++ ZCompilerSettings.cpp
@@ -0,0 +1,1 @@
+﻿#include "ZCompilerSettings.h"
---
+++ ZCompilerSettings.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+
+class ZCompilerSettings {
+public:
+	ZCompilerSettings(int argc, char* args[]) {
+		if (argc <= 1) {
+			addError("No input files");
+			return;
+		}
+		int i = 1;
+
+	}
+
+	bool hasErrors() {
+		return errors.size() > 0;
+	}
+
+	void addError(std::string error) {
+		errors.push_back(error);
+	}
+
+	int optLevel = 1;
+	bool dumpParserTree = false;
+	bool dumpTypingTree = false;
+	bool dumpLlvmBeforeOpt = false;
+	bool dumpLlvmAfterOpt = false;
+
+	std::vector<std::string> sources;
+
+	std::vector<std::string> errors;
+};
