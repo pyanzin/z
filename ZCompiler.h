a67a45d76f99a82d91391f1199ae921678f42905
Igor Pyanzin <igor.pyanzin@gmail.com>
2017-12-13 15:20:46 +0300

    ZCompiler extracted

---
+++ ZCompiler.h
@@ -0,0 +1,124 @@
+﻿#pragma once
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include <cstdio>
+#include <iostream>
+#include "ZLexer.h"
+#include "ZParser.h"
+#include "TypingPass.h"
+#include "LlvmPass.h"
+#include "llvm/ExecutionEngine/ExecutionEngine.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/IR/Verifier.h"
+#include "llvm/Support/TargetRegistry.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetOptions.h"
+#include <fstream>
+#include <sstream>
+#include "ZCompilerSettings.h"
+
+class ZCompiler {
+public:
+	ZCompiler(ZCompilerSettings* settings) {
+		_settings = settings;
+	}
+
+	void run() {
+		try {
+			std::ifstream srcFile;
+			std::stringstream stream;
+			srcFile.open(_settings->sources[0]);
+			stream << srcFile.rdbuf();
+			std::string src = stream.str();
+
+			ZLexer lexer(&src);
+
+			SymbolTable table;
+
+			ZParser parser(lexer, table);
+
+			auto mod = parser.parseModule();
+
+			if (_settings->dumpParserTree)
+				mod->dump(std::cout, 0);
+
+			TypingPass typingPass;
+			typingPass.visit(mod);
+
+			if (_settings->dumpTypingTree)
+				mod->dump(std::cout, 0);
+
+			LlvmPass llvmPass;
+			llvmPass.visit(mod);
+			auto module = llvmPass.getModule();
+
+			if (_settings->dumpLlvmBeforeOpt)
+				module->dump();
+
+			if (_settings->optLevel >= 1) {
+				auto fpm = llvm::make_unique<llvm::legacy::FunctionPassManager>(module);
+				fpm->add(llvm::createPromoteMemoryToRegisterPass());
+				fpm->add(llvm::createInstructionCombiningPass());
+				fpm->add(llvm::createReassociatePass());
+				fpm->add(llvm::createGVNPass());
+				fpm->add(llvm::createCFGSimplificationPass());
+
+				fpm->doInitialization();
+
+				fpm->run(*module->getFunction("main"));
+			}
+
+			if (_settings->dumpLlvmAfterOpt)
+				module->dump();
+
+			InitializeAllTargetInfos();
+			InitializeAllTargets();
+			InitializeAllTargetMCs();
+			InitializeAllAsmParsers();
+			InitializeAllAsmPrinters();
+
+			auto targetTriple = sys::getDefaultTargetTriple();
+			std::string error;
+			auto target = TargetRegistry::lookupTarget(targetTriple, error);
+
+			auto CPU = "generic";
+			auto Features = "";
+
+			TargetOptions opt;
+			auto RM = Reloc::Model();
+			auto TargetMachine = target->createTargetMachine(targetTriple, CPU, Features, opt, RM);
+
+			module->setDataLayout(TargetMachine->createDataLayout());
+			module->setTargetTriple(targetTriple);
+
+			auto Filename = "output.obj";
+			std::error_code EC;
+			raw_fd_ostream dest(Filename, EC, sys::fs::F_None);
+
+			if (EC) {
+				errs() << "Could not open file: " << EC.message();
+				return;
+			}
+
+			legacy::PassManager pass;
+			auto FileType = TargetMachine::CGFT_ObjectFile;
+
+			if (TargetMachine->addPassesToEmitFile(pass, dest, FileType)) {
+				errs() << "TargetMachine can't emit a file of this type";
+				return;
+			}
+
+			pass.run(*module);
+			dest.flush();
+
+		}
+		catch (std::exception ex) {
+			cout << "Error: " + std::string(ex.what()) << '\n';
+		}
+	}
+private:
+	ZCompilerSettings* _settings;
+};
--- main.cpp
+++ main.cpp
@@ -1,128 +1,18 @@
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/Module.h"
-#include <cstdio>
 #include <iostream>
-#include "ZLexer.h"
-#include "ZParser.h"
-#include "TypingPass.h"
-#include "LlvmPass.h"
-#include "llvm/ExecutionEngine/ExecutionEngine.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/Transforms/Scalar.h"
-#include "llvm/IR/Verifier.h"
-#include "llvm/Support/TargetRegistry.h"
-#include "llvm/Support/TargetSelect.h"
-#include "llvm/Target/TargetMachine.h"
-#include "llvm/Target/TargetOptions.h"
-#include <fstream>
-#include <sstream>
+#include "ZCompilerSettings.h"
+#include "ZCompiler.h"
 
 int main(int argc, char* args[]) {
-    try {
-        std::ifstream srcFile;
-        std::stringstream stream;
-        srcFile.open(args[1]);
-        stream << srcFile.rdbuf();
-        std::string src = stream.str();
+	ZCompilerSettings* settings = new ZCompilerSettings(argc, args);
 
-        ZLexer lexer(&src);
+	if (settings->hasErrors()) {
+		for (std::string error : settings->errors)
+			std::cout << error << "\n";
+		return 1;
+	}
 
-        SymbolTable table;
-
-        ZParser parser(lexer, table);
-
-        auto mod = parser.parseModule();
-        mod->dump(std::cout, 0);
-
-        getchar();
-
-        TypingPass typingPass;
-        typingPass.visit(mod);
-        mod->dump(std::cout, 0);
-
-        getchar();
-
-        LlvmPass llvmPass;
-
-        llvmPass.visit(mod);
-
-        auto module = llvmPass.getModule();
-        module->dump();
-
-        verifyModule(*module);
-
-        getchar();
-
-        auto fpm = llvm::make_unique<llvm::legacy::FunctionPassManager>(module);
-        // Promote allocas to registers.
-        fpm->add(llvm::createPromoteMemoryToRegisterPass());
-        //// Do simple "peephole" optimizations and bit-twiddling optzns.
-        fpm->add(llvm::createInstructionCombiningPass());
-        // Reassociate expressions.
-        fpm->add(llvm::createReassociatePass());
-        //// Eliminate Common SubExpressions.
-        fpm->add(llvm::createGVNPass());
-        //// Simplify the control flow graph (deleting unreachable blocks, etc).
-        fpm->add(llvm::createCFGSimplificationPass());
-
-        fpm->doInitialization();
-
-        //fpm->run(*module->getFunction("main"));
-
-        //module->dump();
-
-        InitializeAllTargetInfos();
-        InitializeAllTargets();
-        InitializeAllTargetMCs();
-        InitializeAllAsmParsers();
-        InitializeAllAsmPrinters();
-
-        auto targetTriple = sys::getDefaultTargetTriple();
-        std::string error;
-        auto target = TargetRegistry::lookupTarget(targetTriple, error);
-
-        if (!target) {
-            errs() << Module::Error;
-            return 1;
-        }
-
-        auto CPU = "generic";
-        auto Features = "";
-
-        TargetOptions opt;
-        auto RM = Reloc::Model();
-        auto TargetMachine = target->createTargetMachine(targetTriple, CPU, Features, opt, RM);
-
-        module->setDataLayout(TargetMachine->createDataLayout());
-        module->setTargetTriple(targetTriple);
-
-        auto Filename = "output.obj";
-        std::error_code EC;
-        raw_fd_ostream dest(Filename, EC, sys::fs::F_None);
-
-        if (EC) {
-            errs() << "Could not open file: " << EC.message();
-            return 1;
-        }
-
-        legacy::PassManager pass;
-        auto FileType = TargetMachine::CGFT_ObjectFile;
-
-        if (TargetMachine->addPassesToEmitFile(pass, dest, FileType)) {
-            errs() << "TargetMachine can't emit a file of this type";
-            return 1;
-        }
-
-        pass.run(*module);
-        dest.flush();
-
-        getchar();
-
-    }
-    catch (std::exception ex) {
-        cout << "Error: " + std::string(ex.what()) << '\n';
-        getchar();
-    }
+	ZCompiler* instance = new ZCompiler(settings);
+	instance->run();
+   
     return 0;
 }
